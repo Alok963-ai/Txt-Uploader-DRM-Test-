cdc2f17732a43ef4fb9e62fa2d69f2cf4ef08d7d
Alok963-ai <alokjangde6@gmail.com>
2025-04-27 01:14:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     pass
     """
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7672004632:AAHohyBfssnv2Nlscz0tqiji3r1I7pfsdUc")
-    API_ID = int(os.environ["API_ID", 23069582]
-    API_HASH = os.environ["API_HASH", "b3b56eaf67828684f54d540f684fdf1f"]
-    AUTH_USERS = "1780523256"""
+    API_ID = int(os.environ["API_ID", 20288951]
+    API_HASH = os.environ["API_HASH", "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8"]
+    AUTH_USERS = "7833842279"""
  
